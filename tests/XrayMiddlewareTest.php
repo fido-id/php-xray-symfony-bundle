1c23851a1f2f773ec99aac2df619b72a117ec6d5
Mauro Artizzu <mauro.artizzu@gmail.com>
2023-03-24 13:53:58 +0100

    fix: add stan checks

--- tests/XrayMiddlewareTest.php
+++ tests/XrayMiddlewareTest.php
@@ -8,38 +8,48 @@ use DMS\PHPUnitExtensions\ArraySubset\ArraySubsetAsserts;
 use Fido\PHPXray\HttpSegment;
 use Fido\PHPXray\Segment;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Middleware;
+use Webmozart\Assert\Assert;
 
 class XrayMiddlewareTest extends FunctionalTestCase
 {
     use ArraySubsetAsserts;
 
-    /** @test */
+    /** @test
+     * @throws \Exception
+     * @throws GuzzleException
+     */
     public function will_return_proper_response(): void
     {
+        /* @var HandlerStack $handlerStack */
         $handlerStack = self::getContainer()->get('fido.guzzle.handler_stack');
         $container = [];
         $history = Middleware::history($container);
 
+        Assert::isInstanceOf($handlerStack, HandlerStack::class);
         $handlerStack->push($history);
 
         $client = new Client(['handler' => $handlerStack]);
 
+        /** @var Segment $segment */
         $segment = self::getContainer()->get(Segment::class);
         $segment->setTraceId('1-00000000-000000000000000000000001');
         $client->request(
             'GET',
             'https://httpbin.org/status/200',
-            [],
         );
         $segment->end();
 
+        /** @var Segment[] $subsegments */
         $subsegments = $segment->jsonSerialize()['subsegments'];
         $httpSegment = $subsegments[0]->jsonSerialize();
 
         self::assertCount(1, $subsegments);
         self::assertInstanceOf(HttpSegment::class, $subsegments[0]);
 
+        /** @var array{http: array{request: array{method: string,url: string,},response: array{status: int,}},fault: bool|null,error: bool|null} $httpSegment */
         self::assertEquals('GET', $httpSegment['http']['request']['method']);
         self::assertEquals('https://httpbin.org/status/200', $httpSegment['http']['request']['url']);
         self::assertEquals(200, $httpSegment['http']['response']['status']);
@@ -50,20 +60,30 @@ class XrayMiddlewareTest extends FunctionalTestCase
     /**
      * @test
      * @dataProvider responses_dataprovider
-     * @phpstan-ignore-next-line "Cannot resolve argument $expected of method Tests\XrayMiddlewareTest::will_return_proper_response_with_a_custom_parent_segment()."
+     * @param string $method
+     * @param string $url
+     * @param int $status
+     * @param array{http: array{request: array{method: string,url: string,},response: array{status: int,}},fault: bool|null,error: bool|null} $expected
+     * @throws \Exception
+     * @throws GuzzleException
      */
+
     public function will_return_proper_response_with_a_custom_parent_segment(string $method, string $url, int $status, array $expected): void
     {
+        /* @var HandlerStack $handlerStack */
         $handlerStack = self::getContainer()->get('fido.guzzle.handler_stack');
         $container = [];
         $history = Middleware::history($container);
 
+        Assert::isInstanceOf($handlerStack, HandlerStack::class);
         $handlerStack->push($history);
 
-        $client = new Client(['handler' => $handlerStack, "http_errors" => false]);
+        $client = new Client(['handler' => $handlerStack, 'http_errors' => false]);
 
+        /** @var Segment $parentSegment */
         $parentSegment = self::getContainer()->get(Segment::class);
 
+        /** @var Segment $segment */
         $segment = self::getContainer()->get(Segment::class);
         $segment->setTraceId('1-00000000-000000000000000000000001');
         $client->request(
@@ -74,7 +94,9 @@ class XrayMiddlewareTest extends FunctionalTestCase
 
         $segment->end();
 
+        /** @var Segment[] $subsegments */
         $subsegments = $segment->jsonSerialize()['subsegments'];
+        /** @var array{http: array{request: array{method: string,url: string,},response: array{status: int,}},fault: bool|null,error: bool|null} $httpSegment */
         $httpSegment = $subsegments[0]->jsonSerialize();
 
         self::assertCount(1, $subsegments);
@@ -89,7 +111,8 @@ class XrayMiddlewareTest extends FunctionalTestCase
         self::assertStringMatchesFormat('Root=1-00000000-000000000000000000000001;Parent=%s;Sampled=1', $container[0]['request']->getHeader('X-Amzn-Trace-Id')[0]);
     }
 
-    /* @phpstan-ignore-next-line "Cannot resolve argument $expected of method Tests\XrayMiddlewareTest::will_return_proper_response_with_a_custom_parent_segment()."
+    /**
+     * @return array<string,array{method:string,url:string,status:int,expected:array{http:array{request:array{method:string,url:string,},response:array{status:int,},},fault:bool|null,error:bool|null,}}>
      */
     public function responses_dataprovider(): array
     {
@@ -154,21 +177,26 @@ class XrayMiddlewareTest extends FunctionalTestCase
     /**
      * @test
      * @dataProvider annotations_metadata_dataprovider
-     * @param array<string, array<string, array<string, string>|null>> $annotations
-     * @param array<string, array<string, array<string, string>|null>> $metadata
+     * @param array|null $annotations
+     * @param array|null $metadata
+     * @throws \Exception
+     * @throws GuzzleException
      */
     public function will_populate_segment_with_annotations_and_metadata(?array $annotations, ?array $metadata): void
     {
+        /* @var HandlerStack $handlerStack */
         $handlerStack = self::getContainer()->get('fido.guzzle.handler_stack');
         $container = [];
         $history = Middleware::history($container);
 
+        Assert::isInstanceOf($handlerStack, HandlerStack::class);
         $handlerStack->push($history);
 
         $client = new Client(['handler' => $handlerStack]);
 
         $parentSegment = self::getContainer()->get(Segment::class);
 
+        /** @var Segment $segment */
         $segment = self::getContainer()->get(Segment::class);
         $segment->setTraceId('1-00000000-000000000000000000000001');
         $client->request(
@@ -183,11 +211,14 @@ class XrayMiddlewareTest extends FunctionalTestCase
 
         $segment->end();
 
+        /** @var Segment[] $subsegments */
         $subsegments = $segment->jsonSerialize()['subsegments'];
         $httpSegment = $subsegments[0]->jsonSerialize();
 
-        self::assertArraySubset($annotations, $httpSegment['annotations'] ?? []);
-        self::assertArraySubset($metadata, $httpSegment['metadata'] ?? []);
+        /* @phpstan-ignore-next-line */
+        self::assertArraySubset($annotations, $httpSegment['annotations'] ?? null);
+        /* @phpstan-ignore-next-line */
+        self::assertArraySubset($metadata, $httpSegment['metadata'] ?? null);
     }
 
     /**
